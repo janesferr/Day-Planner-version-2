ccad1e2032685a2ee29addbdbbe9d0793364bf45
jansferr <sferrazzajane@gmail.com>
2021-01-24 00:02:15 -0500

    added the local storage for it to work

--- dayscript.js
+++ dayscript.js
@@ -4,28 +4,28 @@ var dayHour = [
         hour: "9",
         time: "09",
         meridiem: "am",
-        appt: ""
+        appt: " "
     },
     {
         id: "1",
         hour: "10",
         time: "10",
         meridiem: "am",
-        appt: ""
+        appt: " "
     },
     {
         id: "2",
         hour: "11",
         time: "11",
         meridiem: "am",
-        appt: ""
+        appt: " "
     },
     {
         id: "3",
         hour: "12",
         time: "12",
         meridiem: "pm",
-        appt: ""
+        appt: " "
     },
     {
         id: "4",
@@ -39,34 +39,34 @@ var dayHour = [
         hour: "2",
         time: "14",
         meridiem: "pm",
-        appt: ""
+        appt: " "
     },
     {
         id: "6",
         hour: "3",
         time: "15",
         meridiem: "pm",
-        appt: ""
+        appt: " "
     },
     {
         id: "7",
         hour: "4",
         time: "16",
         meridiem: "pm",
-        appt: ""
+        appt: " "
     },
     {
         id: "8",
         hour: "5",
         time: "17",
         meridiem: "pm",
-        appt: ""
+        appt: " "
     },
     
 ]
 
 function dateOntop(){
-    var currentDate = moment().format("dddd, MMMM Do YYYY");
+    var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
     $("#currentDay").text(currentDate);
     
 }
@@ -74,8 +74,8 @@ function dateOntop(){
 
 
 //saving to localStorage
-function saveAppt(event){
-    event.preventDefault();
+function saveAppt(){
+
     localStorage.setItem("dayHour", JSON.stringify(dayHour));
     console.log(dayHour);
 }
@@ -88,14 +88,14 @@ function tellmeAppt(){
 
 //gets what's in the locatStorage data 
 function getStorage() {
-    var getStorage = JSON.parse(localStorage.getItem("dayHour"));
+    var getStoredInfo = JSON.parse(localStorage.getItem("dayHour"));
 
-    if (getStorage){
-        dayHour = getStorage;
+    if (getStoredInfo){
+        dayHour = getStoredInfo;
     }
 
-    saveAppt();
-    tellmeAppt();
+    // saveAppt();
+    // tellmeAppt();
 }
 dateOntop();
 
@@ -108,7 +108,7 @@ dateOntop();
 dayHour.forEach(function(thisHour) {
    
     //create a row with a div tag after the container
-    var hour = $("<div>").attr("class", "row");
+    var hour = $("<form>").attr("class", "row");
     $(".container").append(hour);
 
     //gets the time from the object 
@@ -127,18 +127,27 @@ dayHour.forEach(function(thisHour) {
 
     var momentInTime = moment().format("HH");
         if(thisHour.time < momentInTime) {
-            textData.attr({"class" : "past"})
+            textData.attr("class", "past")
         }
         else if (thisHour.time === momentInTime) {
-            textData.attr("class", ".present")
+            textData.attr("class", "present")
         }
         else if (thisHour.time > momentInTime) {
-            textData.attr("class", ".future")
+            textData.attr("class", "future")
         }
         
     
  // creates the Button
- var saveButton = $("<i class='far fa-save fa-lg'></i>")
+//make an image 
+var saveButton = $("<img>");
+//add a src & alt to the image
+saveButton.attr("src",
+ "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2FBpEgh-exyIaaS3W_W2Z_CeY2Ww5gBML8Q&usqp=CAU")
+ .attr("alt", "pic Save").attr("width", "15%").attr("height", "15%")
+//add a crystal class to it
+
+
+//  var saveButton = $("<i class='far fa-save fa-lg'></i>")
     var savePlan = $("<button>")
         .attr({
             "class": "col-md-1 saveBtn"
@@ -147,18 +156,28 @@ dayHour.forEach(function(thisHour) {
     hour.append(savePlan);
 
 
- })
-
-
-//load data
-// init();
+ });
 
 // saves data to be used in localStorage
+$(document).ready(function(){
 $(".saveBtn").on("click", function(event) {
     event.preventDefault();
+    saveAppt();
+    // getStorage();
+        var getStoredInfo = JSON.parse(localStorage.getItem("dayHour"));
+    
+        if (getStoredInfo){
+            dayHour = getStoredInfo;
+        }
     var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-    dayHour[saveIndex].appt = $(this).siblings(".description").children(".future").val();
     console.log(saveIndex);
-    saveAppt(event);
-    tellmeAppt();
-})
+    dayHour.appt= $(this).siblings(".description").children(".future").val(); 
+    // dayHour[saveIndex].appt = $("<textarea>").val().trim(); 
+    console.log("This is the save Index:"  + saveIndex);
+    console.log(dayHour.appt);
+    // console.log("this is the appt that is beign saved" + dayHour[saveIndex].appt);
+    // saveAppt();
+    // tellmeAppt();
+  });
+});
+
